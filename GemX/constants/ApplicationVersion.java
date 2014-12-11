b682fbb0cfbfacd7c1f36d0721c260d512a5fd04
Radoslaw Golebiewski <radoslaw.golebiewski@simulation-systems.co.uk>
2014-12-12 00:01:45 +0100

    Version change to 10.2.8

--- GemX/constants/ApplicationVersion.java
+++ GemX/constants/ApplicationVersion.java
@@ -3,6 +3,6 @@ package constants;
 public class ApplicationVersion {
 	public static final int verMajor = 10;
 	public static final int verMinor1 = 2;
-	public static final int verMinor2 = 7;
-	public static final int verFix = 4;
+	public static final int verMinor2 = 8;
+	public static final int verFix = 0;
 }
--- ccfx/ccfxconstants.cpp
+++ ccfx/ccfxconstants.cpp
@@ -3,13 +3,13 @@
 #include <boost/cstdint.hpp>
 #include <boost/array.hpp>
 
-const boost::array<boost::int32_t, 4> APPVERSION = { 10, 2, 7, 4 };
+const boost::array<boost::int32_t, 4> APPVERSION = { 10, 2, 8, 0 };
 
 #if defined OS_WIN32
 const std::string PLATFORM_NAME = "Windows XP x86";
 #elif defined OS_UBUNTU
 const std::string PLATFORM_NAME = "Ubuntu i386";
-#elif defined OS_LINUX 
+#elif defined OS_LINUX
 const std::string PLATFORM_NAME = "Linux";
 #else
 #error
